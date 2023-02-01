7b85c83b804482de36afbfa1fae15f4f092f430a
Ghulam Subbhani <gsmalik030@gmail.com>
2023-02-01 17:34:27 +0200

    _Fixed linter errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import "./style.css";
-import { getData,order,refreshList } from "./apiSetting";
-import { postData } from "./apiSetting";
-const form = document.getElementById("form");
-const refreshBtn=document.getElementById('refresh-btn');
-form.addEventListener("submit", postData);
-refreshBtn.addEventListener("click", refreshList);
+import './style.css';
+import {
+  refreshList, postData,
+} from './apiSetting';
+
+const form = document.getElementById('form');
+const refreshBtn = document.getElementById('refresh-btn');
+form.addEventListener('submit', postData);
+refreshBtn.addEventListener('click', refreshList);
