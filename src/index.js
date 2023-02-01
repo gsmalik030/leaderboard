b7797e4dc4750445c08527486a53c23c7ea676ab
Ghulam Subbhani <gsmalik030@gmail.com>
2023-02-01 16:05:51 +0200

    _Created post data and get date functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import './style.css';
+import "./style.css";
+import { getData } from "./apiSetting";
+import { postData } from "./apiSetting";
+const form=document.getElementById("form")
+
+
+form.addEventListener('submit', postData);
+console.log(getData());
